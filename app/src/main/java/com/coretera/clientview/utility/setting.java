09e8cda755d0cba086b1c089bba8f7b14c039c4f
tuck <ch_warayut@hotmail.com>
2019-08-02 03:12:26 +0700

    Fix Coding 7

--- app/src/main/java/com/coretera/clientview/utility/setting.java
+++ app/src/main/java/com/coretera/clientview/utility/setting.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.coretera.clientview.R;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class setting {
 
@@ -33,8 +36,7 @@ public class setting {
     public static boolean isOnline() {
         Runtime runtime = Runtime.getRuntime();
         try {
-            //Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
-            Process ipProcess = runtime.exec("ping  119.59.120.11");
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
             int     exitValue = ipProcess.waitFor();
             return (exitValue == 0);
         }
